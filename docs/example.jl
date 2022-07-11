a1cce51755cb57c44c30a410555bdb35af8b5b40
ShuGe-MIT <geshu@mit.edu>
2022-07-11 17:03:37 -0400

    initial push

---
+++ docs/example.jl
@@ -0,0 +1,43 @@
+using BPGates
+using QuantumClifford
+
+num_bell = 4
+
+##################
+## apply gates
+##################
+
+state = rand_state(num_bell)
+state2 = copy_state(state) # copy the state
+
+gate = BellGateQC(1,14,2,1,1,2)
+# range of inputs: pauli: 1-6, double: 1-20, single1: 1-6, single2: 1-6, index1: 1-num_bell, index2: 1-num_bell
+
+apply_op!(state, gate)
+apply_as_qc!(state2, gate)
+
+@assert state.phases == state2.phases # resultant state after applying gates are the same
+
+res = apply_op!(state, BellMeasure(1,1))[end] # measure index: 1-3, bell index: 1-num_bell
+res2 = apply_as_qc!(state2, BellMeasure(1,1))[end]
+
+@assert res == res2 # measurement results are the same
+
+##################
+## apply circuits
+##################
+
+state = rand_state(num_bell)
+state2 = copy_state(state) # copy the state
+
+circuit = [BellGateQC(1,14,2,1,1,2), BellGateQC(1,13,2,2,1,2)]
+
+apply_op!(state, circuit)
+apply_as_qc!(state2, circuit)
+
+@assert state.phases == state2.phases # resultant state after applying gates are the same
+
+res = apply_op!(state, BellMeasure(1,1))[end] # measure index: 1-3, bell index: 1-num_bell
+res2 = apply_as_qc!(state2, BellMeasure(1,1))[end]
+
+@assert res == res2 # measurement results are the same
--- src/BPGates.jl
+++ src/BPGates.jl
@@ -1,12 +1,19 @@
 module BPGates
 
 using Permutations
+using QuantumClifford
 
-export Int2BitArray, BitArray2Int, stab2qidx, BellState, BellSinglePermutation, BellDoublePermutation, BellPauliPermutation, BellMeasure, BellGate, apply_op!, initialize
+export int_to_bit, bit_to_int, copy_state, BellState, BellSinglePermutation, BellDoublePermutation, BellPauliPermutation, BellMeasure, BellGateQC, apply_op!, rand_state, stab2qidx, apply_as_qc!, convert2QC
 
-Int2BitArray(x,padding)=reverse(BitArray(digits(x, base=2,pad=padding)))
-BitArray2Int(x)=UInt8(reverse(x).chunks[1])
-stab2qidx(stab)=BitArray(stab.phases.÷0x2)
+function int_to_bit(int,digits)
+    int = int - 1 # -1 so that we use julia indexing convenctions
+    Bool[int>>shift&0x1 for shift in 0:digits-1]    
+end
+function bit_to_int(bits)
+    reduce(⊻,(bit<<(index-1) for (index,bit) in enumerate(bits))) + 1 # +1 so that we use julia indexing convenctions
+end
+
+@assert all(bit_to_int(int_to_bit(n,4))==n for n in 1:2^4)
 
 pauli_perm_list = [
 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
@@ -15,8 +22,6 @@ pauli_perm_list = [
 [4, 3, 2, 1, 8, 7, 6, 5, 12, 11, 10, 9, 16, 15, 14, 13]
 ]
 const pauli_perm = [Permutation(p) for p in pauli_perm_list]
-
-test() = 2
 
 one_perm_list = [[1, 2, 3, 4],
  [1, 3, 2, 4],
@@ -77,18 +82,26 @@ end
 Base.getindex(a::AbstractVector, b::BitVector)=a[b.chunks[1]]
 
 ##############################
+# Helper functions
+##############################
+
+function copy_state(state::BellState)
+    return BellState(copy(state.phases))    
+end
+
+##############################
 # Permutations
 ##############################
 
 function apply_op!(state::BellState, op::BellSinglePermutation)
     phase = state.phases
-    phase[op.sidx*2-1:op.sidx*2] = Int2BitArray(one_perm[op.pidx][BitArray2Int(phase[op.sidx*2-1:op.sidx*2])+1]-1,2)
+    phase[op.sidx*2-1:op.sidx*2] = int_to_bit(one_perm[op.pidx][bit_to_int(phase[op.sidx*2-1:op.sidx*2])],2)
     return BellState(phase),:continue
 end
 
 function apply_op!(state::BellState, op::BellDoublePermutation) 
     phase = state.phases
-    changed_phases = Int2BitArray(two_perm[op.pidx][BitArray2Int(vcat(phase[op.sidx[1]*2-1:op.sidx[1]*2],phase[op.sidx[2]*2-1:op.sidx[2]*2]))+1]-1,4)
+    changed_phases = int_to_bit(two_perm[op.pidx][bit_to_int(vcat(phase[op.sidx[1]*2-1:op.sidx[1]*2],phase[op.sidx[2]*2-1:op.sidx[2]*2]))],4)
     phase[op.sidx[1]*2-1:op.sidx[1]*2] = changed_phases[1:2]
     phase[op.sidx[2]*2-1:op.sidx[2]*2] = changed_phases[3:4]
     return BellState(phase),:continue
@@ -96,77 +109,171 @@ end
 
 function apply_op!(state::BellState, op::BellPauliPermutation)
     phase = state.phases
-    changed_phases = Int2BitArray(pauli_perm[op.pidx][BitArray2Int(vcat(phase[op.sidx[1]*2-1:op.sidx[1]*2],phase[op.sidx[2]*2-1:op.sidx[2]*2]))+1]-1,4)
+    changed_phases = int_to_bit(pauli_perm[op.pidx][bit_to_int(vcat(phase[op.sidx[1]*2-1:op.sidx[1]*2],phase[op.sidx[2]*2-1:op.sidx[2]*2]))],4)
     phase[op.sidx[1]*2-1:op.sidx[1]*2] = changed_phases[1:2]
     phase[op.sidx[2]*2-1:op.sidx[2]*2] = changed_phases[3:4]
     return BellState(phase),:continue
 end
 
 ##############################
-# Bell Gates
-##############################
-
-struct BellGate
-    pauli::BellPauliPermutation
-    double::BellDoublePermutation
-    single1::BellSinglePermutation
-    single2::BellSinglePermutation
-end
-
-BellGate(pauli, double, single1, single2, idx1, idx2) = BellGate(BellPauliPermutation(pauli,[idx1,idx2]),
-    BellDoublePermutation(double,[idx1,idx2]),
-    BellSinglePermutation(single1,idx1),
-    BellSinglePermutation(single2,idx2))
-
-function apply_op!(state,bell_gate::BellGate)
-    circuit = [bell_gate.pauli, bell_gate.double, bell_gate.single1, bell_gate.single2]
-    for op in circuit
-        if apply_op!(state,op)[end]==:detected_failure
-            return state, :detected_failure
-        end
-    end
-    return state, :continue
-end
-
-##############################
 # Measurements
 ##############################
 
-mresult=falses(4,3)
-for i in 1:3 
-    mresult[1,i]=true
-    mresult[i+1,i]=true
+mresult = falses(4,3)
+for idx in [[1,1],[1,2],[1,3],[2,3],[3,1],[4,2]]
+    mresult[idx[1],idx[2]] = true
 end
 
 const rmeasurement=mresult
 
 function apply_op!(state::BellState, op::BellMeasure) 
     phase = state.phases
-    result=apply_op!(phase[op.sidx*2-1:op.sidx*2], op)
-    phase[op.sidx*2-1:op.sidx*2]=BitVector([0,0])
+    result = apply_op!(phase[op.sidx*2-1:op.sidx*2], op)
+    phase[op.sidx*2-1:op.sidx*2]=BitVector([0,0]) # reset the measured pair to 00
     if result
         return BellState(phase), :continue
     else
-        return BellState(phase), :detected_failure
+        return BellState(phase), :detected_error
     end
 end
 
 function apply_op!(phase, op::BellMeasure)
-    result=rmeasurement[BitArray2Int(phase)+1,op.midx]
+    result=rmeasurement[bit_to_int(phase),op.midx]
     return result
 end
 
-function apply_op!(state,circuit)
+##############################
+# Full BP gate
+##############################
+
+struct BellGateQC
+    pauli
+    double
+    single1
+    single2
+    idx1
+    idx2
+end
+
+function apply_op!(state, g::BellGateQC)
+    circuit = [
+    BellPauliPermutation(g.pauli,[g.idx1,g.idx2]),
+    BellDoublePermutation(g.double,[g.idx1,g.idx2]),
+    BellSinglePermutation(g.single1,g.idx1),
+    BellSinglePermutation(g.single2,g.idx2)
+    ]
     for op in circuit
-        if apply_op!(state,op)[end]==:detected_failure
-            return state, :detected_failure
+        if apply_op!(state,op)[end]==:detected_error
+            return state, :detected_error
         end
     end
     return state, :continue
 end
 
-function initialize(num_bell) 
-    return BellState(Int2BitArray(rand(1:4^num_bell),num_bell*2)) 
+function apply_op!(state,circuit)
+    for op in circuit
+        if apply_op!(state,op)[end]==:detected_error
+            return state, :detected_error
+        end
+    end
+    return state, :continue
+end
+
+##############################
+# Convertion from BP to QC
+##############################
+
+stab2qidx(stab)=isone.(stab.phases.÷0x2)
+
+const one_perm_qc = [C"X Z",
+C"Z X",
+C"Y X",
+C"X Y",
+C"Z Y",
+C"Y Z"];
+
+const two_perm_qc = [C"X_ _Z Z_ _X",
+C"_X X_ _Z Z_",
+C"XX _X Z_ ZZ",
+C"ZX _X X_ XZ",
+C"XZ X_ _X ZX",
+C"ZZ XX X_ _Z",
+C"ZY XX X_ _Y",
+C"XX _X ZX YY",
+C"_Z X_ XX ZZ",
+C"XZ X_ XX YY",
+C"ZZ XX _X Z_",
+C"YZ XX _X Y_",
+C"Z_ ZX XZ _Z",
+C"Y_ YX XZ _Z",
+C"ZX Z_ _Z XZ",
+C"YX Y_ _Z XZ",
+C"_Y XY ZX Z_",
+C"XY _Y Z_ ZX",
+C"ZY YZ XY _Y",
+C"YX Y_ _Y ZY"];
+
+const pauli_perm_qc = [P"II",P"XI",P"ZI",P"YI"];
+
+function rand_state(num_bell) 
+    return BellState(int_to_bit(rand(1:4^num_bell),num_bell*2)) 
+end
+
+function convert2QC(gate::BellGateQC)
+    return [
+        (pauli_perm_qc[gate.pauli], [gate.idx1*2-1, gate.idx2*2-1]),
+        (two_perm_qc[gate.double], [gate.idx1*2-1, gate.idx2*2-1]),
+        (two_perm_qc[gate.double], [gate.idx1*2, gate.idx2*2]),
+        (one_perm_qc[gate.single1], [gate.idx1*2-1]),
+        (one_perm_qc[gate.single1], [gate.idx1*2]),
+        (one_perm_qc[gate.single2], [gate.idx2*2-1]),
+        (one_perm_qc[gate.single2], [gate.idx2*2]),
+    ]
+end
+
+function convert2QC(state::BellState)
+    res = bell(length(state.phases)÷2)
+    res.phases .= state.phases*0x2
+    return res
+end
+function convert2BP(state::Stabilizer)
+    return BellState(stab2qidx(state))
+end
+
+function apply_as_qc!(state::BellState, gate::BellGateQC)
+    s = convert2QC(state)
+    for (g, idx) in convert2QC(gate)
+        apply!(s, g, idx)
+    end
+    new_phases = [project!(s,proj)[end]÷2 for proj in bell(length(state.phases)÷2)]
+    state.phases .= new_phases
+    return state, :continue
+end
+
+
+function apply_as_qc!(state::BellState, gate::BellMeasure)
+    phases = state.phases[:]
+    s = MixedDestabilizer(convert2QC(state))
+    if gate.midx == 1
+        res = (projectXrand!(s,gate.sidx*2-1)[2]==projectXrand!(s,gate.sidx*2)[2]) ? :continue : :detected_error
+        
+    elseif gate.midx == 2
+        res = !(projectYrand!(s,gate.sidx*2-1)[2]==projectYrand!(s,gate.sidx*2)[2]) ? :continue : :detected_error
+    else
+        res = (projectZrand!(s,gate.sidx*2-1)[2]==projectZrand!(s,gate.sidx*2)[2]) ? :continue : :detected_error
+    end
+    phases[gate.sidx*2-1:gate.sidx*2] .=0
+    state.phases .= phases
+    return state, res
+end
+
+function apply_as_qc!(state::BellState, circuit)
+    for op in circuit
+        if apply_as_qc!(state,op)[end]==:detected_error
+            return state, :detected_error
+        end
+    end
+    return state, :continue
 end
 
 end # module
