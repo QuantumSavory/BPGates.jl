5fbd5d5d7625980f67cf77b510b6b1f959be525c
Stefan Krastanov <stefan@krastanov.org>
2022-07-12 17:25:05 -0400

    Misc
    - Make the structs have non-Any slots
    - Use tuples instead of vectors
    - Avoid creating temporary lists
    
    This lead to significant performance improvements in
    `apply!(state,::BellGateQC)`
    
    Also use the `Base.copy` method instead of creating a custom one.

--- docs/example.jl
+++ docs/example.jl
@@ -1,3 +1,5 @@
+using Revise
+using BenchmarkTools
 using BPGates
 using QuantumClifford
 
@@ -8,7 +10,7 @@ num_bell = 4
 ##################
 
 state = rand_state(num_bell)
-state2 = copy_state(state) # copy the state
+state2 = copy(state) # copy the state
 
 gate = BellGateQC(1,14,2,1,1,2)
 # range of inputs: pauli: 1-6, double: 1-20, single1: 1-6, single2: 1-6, index1: 1-num_bell, index2: 1-num_bell
@@ -28,7 +30,7 @@ res2 = apply_as_qc!(state2, BellMeasure(1,1))[end]
 ##################
 
 state = rand_state(num_bell)
-state2 = copy_state(state) # copy the state
+state2 = copy(state) # copy the state
 
 circuit = [BellGateQC(1,14,2,1,1,2), BellGateQC(1,13,2,2,1,2)]
 
