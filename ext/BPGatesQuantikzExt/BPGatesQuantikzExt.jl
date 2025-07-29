dbb75842ad04e86e750983ce466c88d442f99115
Isaac Pelenur <98821058+xoth42@users.noreply.github.com>
2025-07-29 17:22:52 -0400

    v1.2.2: Compat with QEPOptimize, Fix visualization of T1, T2 for Quantikz extention, add affectedqubits() (#31)
    * add T1 T2 visualizations
    
    * affectedqubits(): return qubits from operation
    
    * update project.toml version 1.2.2
    
    ---------
    
    Co-authored-by: xoth42 <>

--- ext/BPGatesQuantikzExt/BPGatesQuantikzExt.jl
+++ ext/BPGatesQuantikzExt/BPGatesQuantikzExt.jl
@@ -33,5 +33,22 @@ Quantikz.QuantikzOp(op::PauliNoiseBellGate) = Quantikz.QuantikzOp(op.g)
 Quantikz.QuantikzOp(op::NoisyBellMeasure) = Quantikz.QuantikzOp(op.m)
 Quantikz.QuantikzOp(op::NoisyBellMeasureNoisyReset) = Quantikz.QuantikzOp(op.m)
 
+# Add t1 and t2 noise op visualizations, to show as a gate with 'T1' and 'T2' labels, and also the λ values in the label
+Quantikz.QuantikzOp(op::T1NoiseOp) = Quantikz.U(small_vert_matrix(("\\mathcal{\\lambda}_1",round(op.λ₁, digits=2)), "\\ T_1"), op.idx)
+Quantikz.QuantikzOp(op::T2NoiseOp) = Quantikz.U(small_vert_matrix(("\\mathcal{\\lambda}_2",round(op.λ₂, digits=2)), "\\ T_2"), op.idx)
+
+# extend Quantikz.affectedqubits to include the new gates
+# Get the qubit pairs involved in an operation
+Quantikz.affectedqubits(gate::PauliNoiseBellGate) = [gate.g.idx1, gate.g.idx2]
+Quantikz.affectedqubits(gate::NoisyBellMeasureNoisyReset) = [gate.m.sidx,]
+Quantikz.affectedqubits(gate::BellMeasure) = [gate.sidx,]
+Quantikz.affectedqubits(gate::CNOTPerm) = [gate.idx1, gate.idx2]
+Quantikz.affectedqubits(gate::GoodSingleQubitPerm) = [gate.idx,]
+Quantikz.affectedqubits(gate::BellSinglePermutation) = [gate.sidx,]
+Quantikz.affectedqubits(gate::BellPauliPermutation) = [gate.sidx,]
+Quantikz.affectedqubits(gate::T1NoiseOp) = [gate.idx,]
+Quantikz.affectedqubits(gate::T2NoiseOp) = [gate.idx,]
+
+
 
 end
--- test/test_quantikz.jl
+++ test/test_quantikz.jl
@@ -5,8 +5,17 @@ using Quantikz
 using Test
 
 @testset "Quantikz" begin
-    quantikz_circ = Quantikz.QuantikzOp.([BellSinglePermutation(2,2), BellDoublePermutation(1,2,3), BellPauliPermutation(4,1), BellMeasure(3,1), BellGate(1,1,1,1,1,1,2), CNOTPerm(1,2,3,4), GoodSingleQubitPerm(1,2), PauliNoiseOp(1,0.0,0.0,0.0), PauliNoiseBellGate(CNOTPerm(1,2,3,4),0.0,0.0,0.0), NoisyBellMeasure(BellMeasure(1,2),0.0), NoisyBellMeasureNoisyReset(BellMeasure(1,2),0.0,0.0,0.0,0.0)])
+    quantikz_circ = Quantikz.QuantikzOp.([BellSinglePermutation(2,2), BellDoublePermutation(1,2,3), BellPauliPermutation(4,1), BellMeasure(3,1), BellGate(1,1,1,1,1,1,2), CNOTPerm(1,2,3,4), GoodSingleQubitPerm(1,2), PauliNoiseOp(1,0.0,0.0,0.0), PauliNoiseBellGate(CNOTPerm(1,2,3,4),0.0,0.0,0.0), NoisyBellMeasure(BellMeasure(1,2),0.0), NoisyBellMeasureNoisyReset(BellMeasure(1,2),0.0,0.0,0.0,0.0),T1NoiseOp(1,0.1),T2NoiseOp(2,0.2)])
     img = Quantikz.circuit2image(quantikz_circ)
+end
+@testset "affectedqubits extension" begin
+    using Quantikz:affectedqubits
+    # using BPGates:affectedqubits
+    # Should return the correct bell pairs for each gate
+    circ = [BellSinglePermutation(2,2), BellDoublePermutation(1,2,3), BellPauliPermutation(4,1), BellMeasure(3,1), BellGate(1,1,1,1,1,1,2), CNOTPerm(1,2,3,4), GoodSingleQubitPerm(1,2), PauliNoiseOp(1,0.0,0.0,0.0), PauliNoiseBellGate(CNOTPerm(1,2,3,4),0.0,0.0,0.0), NoisyBellMeasure(BellMeasure(1,2),0.0), NoisyBellMeasureNoisyReset(BellMeasure(1,2),0.0,0.0,0.0,0.0),T1NoiseOp(1,0.1),T2NoiseOp(2,0.2)]
+
+    pairs = map(affectedqubits,circ)
+    @test pairs == [[2],[2,3],[1],[1],[1,2],[3,4],[2],[1],[3,4], [2], [2],[1],[2]]
 end
 
 end
